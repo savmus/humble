229275169dfbcf387fb1293f9bd3bfe99737bda5
Savannah <savannahmusladin@yahoo.com>
2019-07-29 12:40:06 -0700

    Finished reducers

---
+++ frontend/reducers/session_errors_reducer.js
@@ -0,0 +1,11 @@
+import { RECEIVE_ERRORS } from '../actions/session_actions';
+
+export default (state = [], action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_ERRORS:
+            return action.errors;
+        default:
+            return state;
+    }
+};
