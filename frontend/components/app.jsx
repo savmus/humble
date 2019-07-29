3910cdcf20301337f1c628642de59f82d726de40
Savannah <savannahmusladin@yahoo.com>
2019-07-29 12:29:13 -0700

    Changed entry file, root and app components, and created AJAX requests

---
+++ frontend/components/app.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './session/login_form_container';
+import SignupFormContainer from './session/signup_form_container';
+import { Route } from 'react-router-dom';
+import { AuthRoute } from '../util/route_util';
+
+const App = () => (
+    <div>
+        <header>
+            <h1>humble</h1>
+            <GreetingContainer />
+        </header>
+
+        <AuthRoute path="/login" component={LoginFormContainer} />
+        <AuthRoute path="/signup" component={SignupFormContainer} />
+    </div>
+);
+
+export default App;
