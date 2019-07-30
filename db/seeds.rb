13ae7da08a5642aa65b8481341e14780d7c34e40
Savannah <savannahmusladin@yahoo.com>
2019-07-30 14:04:43 -0700

    Added styling and db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(username: "demo", email: "demo@demo.com", password: "12345678")
+User.create(username: "sav", email: "sav@hotmail.com", password: "87654321")
+User.create(username: "11111", email: "11111@outlook.com", password: "password")
+User.create(username: "abc123", email: "abc123@gmail.com", password: "123456789")
+User.create(username: "catchthismail", email: "catchthismail@yahoo.com", password: "12345678")
+User.create(username: "fabrika", email: "fabrika@inbox.com", password: "sunshine")
+User.create(username: "job", email: "job@mail.com", password: "iloveyou")
+User.create(username: "office", email: "office@outlook.com", password: "princess")
+User.create(username: "uploader", email: "uploader@gmail.com", password: "football")
+User.create(username: "baranov", email: "baranov@yahoo.com", password: "!@#$%^&*")
