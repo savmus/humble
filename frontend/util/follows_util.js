76ff1910be181f11fb9cc6f91a3e6d4f3409843e
Savannah <savannahmusladin@yahoo.com>
2019-08-06 18:37:26 -0700

    Fixed some navigation issues

--- frontend/util/follows_util.js
+++ frontend/util/follows_util.js
@@ -2,13 +2,19 @@ export const createFollow = follow => (
     $.ajax({
         url: `/api/blogs/${follow.followee_id}`,
         method: 'POST',
-        data: { follow }
+        data: { follow },
+        success: (data) => {
+            location.reload();
+        }
     })
 );
 
 export const deleteFollow = id => (
     $.ajax({
         url: `/api/blogs/${id}`,
-        method: 'DELETE'
+        method: 'DELETE',
+        success: (data) => {
+            location.reload();
+        }
     })
 );
